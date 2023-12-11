40080d4f91f980572cbdce0b70225749505899ab
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-12-11 15:36:43 -0600

    Got the fullname corrected

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ class Employee {
     this.jobTitle = jobTitle;
     this.payRate = payRate;
     }
-    getFullName(){
-    return this.firtName+""+this.lastName; 
+    fullName(){
+        let fullName=this.firstName +" "+ this.lastName;
+        return  fullName;
     }
     promote(newJobTitle,newPayRate){
         this.jobTitle=newJobTitle;
         this.payRate=newPayRate;
+    }
+    getintro(){
+        let intro= "Hi! I'm " + this.fullName()+" and I am a "+this.jobTitle;
+        return intro
     }
     
 }
@@ -19,8 +24,11 @@ class Employee {
 //let employee1 = new Employee(1,"Ian","Auston","Graphic Artist",42.50);
 //console.log(`Employee ${employee1.firstName} created`)    
 let employee1 = new Employee(1,"Ian","Auston","Graphic Artist",42.50);
-
-console.log(`Employee ${employee1.fullName} created`);
+let fullName = employee1.fullName();
+let intro = employee1.getintro();
+console.log(fullName)
+console.log(intro)
+console.log(`Employee ${employee1.fullName()} created`);
 employee1.promote("Sr. Graphic Artist", 46.75)
 console.log(`Job title is ${employee1.jobTitle}`);
 console.log(`Pay rate is $${employee1.payRate}`);
